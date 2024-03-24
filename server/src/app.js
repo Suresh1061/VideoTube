17ccf71e6f60291b2354d489f30faf49b0ba28e0
Suresh Pal <sureshabantika@gmail.com>
2024-03-25 03:30:30 +0530

    modifed app.js

--- server/src/app.js
+++ server/src/app.js
@@ -6,7 +6,7 @@ const app = express()
 
 app.use(cors({
     credentials: true,
-    origin: "https://youtube-clone-e1ow.onrender.com/api/v1",
+    origin: process.env.CORS_ORIGIN,
 }))
 
 app.use(express.json({ limit: '16kb' }))
