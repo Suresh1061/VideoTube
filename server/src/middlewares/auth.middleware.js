bb3a29540e82991bce7e203979e739295a1fa67d
Suresh Pal <sureshabantika@gmail.com>
2024-03-25 23:56:35 +0530

    debug

--- server/src/middlewares/auth.middleware.js
+++ server/src/middlewares/auth.middleware.js
@@ -4,13 +4,45 @@ import jwt from "jsonwebtoken"
 import { asyncHandler } from "../utils/asyncHandler.js"
 
 
-export const verifyJWT = asyncHandler(async (req, _, next) => { 
+// export const verifyJWT = asyncHandler(async (req, _, next) => { 
+//     console.log(req.cookies)
+//     console.log(req.header("Authorization"))
+//     try {
+//         const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "")
+//         console.log(token)
+//         if (!token) {
+//             throw new ApiError(401, "Unauthorized request")
+//         }
+
+//         const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
+//         console.log(decodedToken);
+
+//         const user = await User.findById(decodedToken?._id).select("-password -refreshToken")
+//         console.log(user)
+
+//         if (!user) {
+//             throw new ApiError(401, "invalid access token")
+//         }
+
+//         req.user = user
+//         next()
+//     } catch (error) {
+//         console.log(error)
+//         throw new ApiError(401, error?.message || "Invalid access token")
+//     }
+// })
+
+export const verifyJWT = asyncHandler(async (req, _, next) => {
+    console.log("cookies start : ")
     console.log(req.cookies)
-    console.log(req.header("Authorization"))
+    console.log(req.cookies.accessToken)
+    console.log(req.cookies.refreshToken)
+    console.log("cookies end : ")
+    
     try {
-        const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "")
-        console.log(token)
-        if (!token) {
+        let token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "")
+        // Fallback mechanism: prioritize headers over cookies if cookies are not available
+        if (!token && !req.cookies) {
             throw new ApiError(401, "Unauthorized request")
         }
 
@@ -27,6 +59,7 @@ export const verifyJWT = asyncHandler(async (req, _, next) => {
         req.user = user
         next()
     } catch (error) {
+        console.log("print error")
         console.log(error)
         throw new ApiError(401, error?.message || "Invalid access token")
     }
